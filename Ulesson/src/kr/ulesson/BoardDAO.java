f7e238f2a0552b4cd1105eef75a9a9801b5baf9d
SIST <SIST@DESKTOP-QTCJ65Q>
2025-02-21 19:25:46 +0900

    hello

--- Ulesson/src/kr/ulesson/BoardDAO.java
+++ Ulesson/src/kr/ulesson/BoardDAO.java
@@ -3,10 +3,111 @@ package kr.ulesson;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 import kr.util.DBUtil;
 
 public class BoardDAO {
+	
+	// 게시글 작성자 확인
+	
+	public boolean isWriter(int bdNum, String memId) {
+	    Connection conn = null;
+	    PreparedStatement pstmt = null;
+	    ResultSet rs = null;
+	    String sql = null;
+
+	    try {
+	        conn = DBUtil.getConnection();
+	        
+	        sql = "SELECT mem_id FROM board WHERE bd_num = ?"; // 작성자 확인
+	        pstmt = conn.prepareStatement(sql);
+	        pstmt.setInt(1, bdNum);
+	        rs = pstmt.executeQuery();
+
+	        if (rs.next()) {
+	            String writerId = rs.getString("mem_id");
+	            
+	            return writerId.equals(memId);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        DBUtil.executeClose(rs, pstmt, conn);
+	    }
+	    return false;  
+	}
+	
+	
+	// 게시판(글) 번호 존재 여부 확인
+    public boolean isBoardNumberExist(int bdNum) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT COUNT(*) FROM board WHERE bd_num = ?"; 
+
+        try {
+            conn = DBUtil.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bdNum);
+            rs = pstmt.executeQuery();
+
+            if (rs.next() && rs.getInt(1) > 0) {
+                return true;  // 존재하는 게시판(글) 번호
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.executeClose(rs, pstmt, conn);
+        }
+        return false;  // 존재하지 않는 게시판(글) 번호
+    }
+	
+	// 회원 아이디 존재 여부 확인
+    public boolean isMemberExist(String memId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT COUNT(*) FROM member WHERE mem_id = ?"; 
+
+        try {
+            conn = DBUtil.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next() && rs.getInt(1) > 0) {
+                return true;  // 존재하는 회원 ID
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.executeClose(rs, pstmt, conn);
+        }
+        return false;  // 존재하지 않는 회원 ID
+    }
+    
+ // 카테고리 존재 여부 확인
+    public boolean isBDCT_numExist(int bdCategory) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT COUNT(*) FROM board_category WHERE bdct_num = ?"; 
+
+        try {
+            conn = DBUtil.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bdCategory);
+            rs = pstmt.executeQuery();
+
+            if (rs.next() && rs.getInt(1) > 0) {
+                return true;  // 존재하는 카테고리 넘버
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.executeClose(rs, pstmt, conn);
+        }
+        return false;  // 존재하지 않는 카테고리 넘버
+    }
 
     // 글쓰기
     public void insertBoard(String memId, String bdContent, int bdCategory) {
@@ -15,15 +116,20 @@ public class BoardDAO {
         String sql = null;
         try {
             conn = DBUtil.getConnection();
-            sql = "INSERT INTO board (mem_id, bd_content, bd_category, bd_date) "
-                + "VALUES (?, ?, ?, SYSDATE)";
+            
+            
+            sql = "INSERT INTO board (bd_num,mem_id, bd_content, bd_category, bd_date) "
+                + "VALUES (board_seq.NEXTVAL,?, ?, ?, SYSDATE)";
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, memId);
             pstmt.setString(2, bdContent);
             pstmt.setInt(3, bdCategory);
 
             int count = pstmt.executeUpdate();
-            System.out.println(count + "개의 행을 삽입했습니다.");
+           
+            	System.out.println(count + "개의 글을 추가했습니다.");
+            	System.out.println("------------------------------------------");
+		
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -46,12 +152,9 @@ public class BoardDAO {
 
             System.out.println("--------------- 게시글 목록 ---------------");
             while (rs.next()) {
-                System.out.println("번호: " + rs.getInt("bd_num"));
-                System.out.println("작성자: " + rs.getString("mem_id"));
-                System.out.println("내용: " + rs.getString("bd_content"));
-                System.out.println("카테고리: " + rs.getInt("bd_category"));
-                System.out.println("작성일: " + rs.getDate("bd_date"));
-                System.out.println("----------------------------------------");
+                System.out.println("글번호: " + rs.getInt("bd_num"));
+                
+
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -69,7 +172,10 @@ public class BoardDAO {
 
         try {
             conn = DBUtil.getConnection();
-            sql = "SELECT * FROM board WHERE bd_num = ?";
+            sql = "SELECT b.bd_num, b.mem_id, b.bd_content, b.bd_category, b.bd_date, b.bd_recommend, bc.bdct_name " +
+                    "FROM board b " +
+                    "JOIN Board_Category bc ON b.bd_category = bc.bdct_num " +
+                    "WHERE b.bd_num = ?";
             pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1, bdNum);
             rs = pstmt.executeQuery();
@@ -78,10 +184,11 @@ public class BoardDAO {
                 System.out.println("글번호: " + rs.getInt("bd_num"));
                 System.out.println("작성자: " + rs.getString("mem_id"));
                 System.out.println("내용: " + rs.getString("bd_content"));
-                System.out.println("카테고리: " + rs.getInt("bd_category"));
+                System.out.println("카테고리: " + rs.getString("bdct_name"));
                 System.out.println("작성일: " + rs.getDate("bd_date"));
+                System.out.println("추천수:" + rs.getInt("bd_recommend"));
             } else {
-                System.out.println("해당 번호의 게시글이 없습니다.");
+                System.out.println("해당 번호의 게시글이 존재하지 않습니다.");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -97,6 +204,7 @@ public class BoardDAO {
         String sql = null;
 
         try {
+            // 데이터베이스 연결
             conn = DBUtil.getConnection();
             // 게시글 내용 수정
             sql = "UPDATE board SET bd_content = ? WHERE bd_num = ?";
@@ -110,12 +218,14 @@ public class BoardDAO {
             } else {
                 System.out.println("해당 게시글이 존재하지 않습니다.");
             }
-        } catch (Exception e) {
+         
+        }catch (Exception e) {
             e.printStackTrace();
         } finally {
             DBUtil.executeClose(null, pstmt, conn);
         }
     }
+
 
     // 글 삭제
     public void deleteBoard(int bdNum) {
@@ -129,11 +239,19 @@ public class BoardDAO {
             pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1, bdNum);
             int count = pstmt.executeUpdate();
-            System.out.println(count + "개의 행을 삭제했습니다.");
+            if (count>0) {
+            	System.out.println("게시글 " + bdNum + "번이 삭제되었습니다.");
+			}else {
+				 System.out.println("해당 게시글이 존재하지 않습니다.");
+			}
+            
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             DBUtil.executeClose(null, pstmt, conn);
         }
     }
+    
+    
+
 }
--- Ulesson/src/kr/ulesson/BoardMain.java
+++ Ulesson/src/kr/ulesson/BoardMain.java
@@ -6,99 +6,188 @@ import java.io.InputStreamReader;
 
 public class BoardMain {
 
-    private BufferedReader br;
-    private BoardDAO boardDAO;
+	private BufferedReader br;
+	private BoardDAO boardDAO;
 
-    public BoardMain() {
-        try {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            boardDAO = new BoardDAO();
-            // 메뉴 호출
-            callMenu();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // 자원 정리
-            if (br != null)
-                try {
-                    br.close();
-                } catch (IOException e) {
-                }
-        }
-    }
+	public BoardMain() {
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			boardDAO = new BoardDAO();
+			// 메뉴 호출
+			callMenu();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 자원 정리
+			if (br != null)
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+		}
+	}
 
-    // 메뉴
-    private void callMenu() throws IOException {
-        while (true) {
-            System.out.print("1.글쓰기, 2.목록보기, 3.상세글보기, 4.글수정, 5.글삭제, 6.종료>");
-            try {
-                int no = Integer.parseInt(br.readLine());
+	// 메뉴
+	private void callMenu() throws IOException {
+		while (true) {
 
-                if (no == 1) {
-                    // 글쓰기
-                    System.out.print("회원 ID: ");
-                    String memId = br.readLine();
+			System.out.print("1.글쓰기, 2.목록보기, 3.상세글보기, 4.글수정, 5.글삭제, 6.종료>");
+			try {
+				int no = Integer.parseInt(br.readLine());
 
-                    System.out.print("게시글 내용: ");
-                    String bdContent = br.readLine();
+				if (no == 1) {
+					// 글쓰기
+					String memId = null;
 
-                    System.out.print("카테고리 번호: ");
-                    int bdCategory = Integer.parseInt(br.readLine());
+					while (true) {
+						System.out.print("회원 ID: ");
+						memId = br.readLine();
 
-                    // BoardDAO의 insertBoard 메서드를 호출해서 입력받은 데이터 전달
-                    boardDAO.insertBoard(memId, bdContent, bdCategory);
+						if (!boardDAO.isMemberExist(memId)) {
+							System.out.println("------------------------------------------");
+							System.out.println("잘못된 회원 ID입니다. 다시 입력해주세요.");
+						} else {
+							break; 
+						}
+					}
 
-                } else if (no == 2) {
-                    // 목록보기
-                    boardDAO.selectAllBoards();
-  
-                } else if (no == 3) {
-                    // 상세글보기
-                    boardDAO.selectAllBoards();
+					BoardCategoryDAO.selectAllBoardCategories();
 
-                    System.out.print("선택한 글의 번호: ");
-                    int bdNum = Integer.parseInt(br.readLine());
+					int bdCategory =0;
 
-                    System.out.println("------------------");
+					while(true) {
+						System.out.println("------------------------------------------");
+						System.out.print("카테고리 번호를 선택하시오>");										
+						try {
+							bdCategory = Integer.parseInt(br.readLine());
+							if (!boardDAO.isBDCT_numExist(bdCategory)) {
+								System.out.println("존재하지 않는 카테고리 번호입니다. 다시 입력해주세요.");		                       
+							}else {
+								break;
+							}
 
-                    boardDAO.selectBoardDetail(bdNum);
+						} catch (NumberFormatException e) {
+							System.out.println("숫자만 입력 가능. 다시 입력해주세요.");
+						}
 
-                } else if (no == 4) {
-                    // 글수정
-                    
+					}
+					System.out.println("------------------------------------------");
+					System.out.print("게시글 내용: ");
+					String bdContent = br.readLine();
 
-                    System.out.print("수정할 글의 번호: ");
-                    int bdNum = Integer.parseInt(br.readLine());
-                    System.out.print("게시글 내용: ");
-                    String bdContent = br.readLine();
-                  
+					boardDAO.insertBoard(memId, bdContent, bdCategory);
 
-                    
-                    boardDAO.updateBoard(bdNum, bdContent);
+				} else if (no == 2) {
+					// 목록보기
+					boardDAO.selectAllBoards();
+					System.out.println("------------------------------------------");
 
-                } else if (no == 5) {
-                    // 글삭제
-                    boardDAO.selectAllBoards();
+				} else if (no == 3) {
+					// 상세글보기
+					boardDAO.selectAllBoards();
 
-                    System.out.print("삭제할 글의 번호: ");
-                    int bdNum = Integer.parseInt(br.readLine());
 
-                    boardDAO.deleteBoard(bdNum);
+					System.out.println("------------------------------------------");
+					int bdNum = 0;
+					while (true) {
+						try {
+							System.out.print("선택할 게시글의 번호: ");
+							bdNum = Integer.parseInt(br.readLine());
+							System.out.println("------------------------------------------");
+							// 게시글 존재 확인
+							if (!boardDAO.isBoardNumberExist(bdNum)) {
+								System.out.println("존재하지 않는 글 번호입니다. 다시 입력해주세요.");							
+							}else {
+								break;
+							}			
+						} catch (NumberFormatException e) {
+							System.out.println("숫자만 입력 가능합니다. 다시 시도해주세요.");
+						}
+					}
+					boardDAO.selectBoardDetail(bdNum);
 
-                } else if (no == 6) {
-                    // 종료
-                    System.out.println("프로그램을 종료합니다.");
-                    break;
-                } else {
-                    System.out.println("잘못 입력했습니다.");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("[숫자만 입력 가능]");
-            }
-        }
-    }
+				} else if (no == 4) {
 
-    public static void main(String[] args) {
-        new BoardMain();
-    }
+					// 글수정
+
+					while (true) {
+						System.out.print("회원 ID: ");
+						String memId = br.readLine();
+
+						if (!boardDAO.isMemberExist(memId)) {
+							System.out.println("잘못된 회원 ID입니다. 다시 입력해주세요.");
+						} else {
+							break;
+						}
+					}
+					int bdNum = 0;
+					while(true) {
+						try {
+							System.out.print("수정할 글의 번호: ");
+							bdNum = Integer.parseInt(br.readLine());
+							// 게시글 존재 확인
+							if (!boardDAO.isBoardNumberExist(bdNum)) {
+								System.out.println("존재하지 않는 글 번호입니다. 다시 입력해주세요.");						
+							}else {
+
+								break;
+							}
+						} catch (Exception e) {
+							System.out.println("숫자만 입력 가능합니다. 다시 시도해주세요.");
+						}
+					}
+					System.out.print("게시글 내용: ");
+
+					String bdContent = br.readLine();
+					boardDAO.updateBoard(bdNum, bdContent);					
+
+				} else if (no == 5) {
+					// 글삭제
+
+					while (true) {
+						System.out.print("회원 ID: ");
+						String memId = br.readLine();
+
+						if (!boardDAO.isMemberExist(memId)) {
+							System.out.println("잘못된 회원 ID입니다. 다시 입력해주세요.");
+						} else {
+							break; 
+						}
+					}
+
+					boardDAO.selectAllBoards();
+
+					int bdNum=0;
+					while(true) {
+						try {
+							System.out.print("삭제할 글의 번호: ");
+							bdNum = Integer.parseInt(br.readLine());
+							//게시글 존재 확인
+							if (!boardDAO.isBoardNumberExist(bdNum)) {
+								System.out.println("존재하지 않는 글 번호입니다. 다시 입력해주세요.");						
+							}else {
+								break;
+							}
+						} catch (Exception e) {
+							System.out.println("숫자만 입력 가능합니다. 다시 시도해주세요.");
+						}
+					}
+					boardDAO.deleteBoard(bdNum);			
+
+				} else if (no == 6) {
+					// 종료
+					System.out.println("프로그램을 종료합니다.");
+					break;
+				} else {
+					System.out.println("잘못 입력했습니다.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("[숫자만 입력 가능]");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new BoardMain();
+	}
 }
